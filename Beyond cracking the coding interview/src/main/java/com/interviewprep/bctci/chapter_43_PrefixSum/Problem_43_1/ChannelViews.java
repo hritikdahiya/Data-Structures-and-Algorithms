3db9590d06c52a7a41215565aa21496140530806
Hritik Dahiya <hritikdahiya@gmail.com>
2025-10-02 17:09:58 +0530

    Problem 43.1 - Channel views

---
+++ Beyond cracking the coding interview/src/main/java/com/interviewprep/bctci/chapter_43_PrefixSum/Problem_43_1/ChannelViews.java
@@ -0,0 +1,23 @@
+package com.interviewprep.bctci.chapter_43_PrefixSum.Problem_43_1;
+
+public class ChannelViews {
+    public int[] getViews(int[] views, int[][] queries) {
+        int n = views.length;
+        int[] prefixViews = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefixViews[i + 1] = prefixViews[i] + views[i];
+        }
+
+        int q = queries.length;
+        int[] responses = new int[q];
+        int i = 0;
+        for (int[] query : queries) {
+            int l = query[0], r = query[1];
+            assert l >= 0 && l < n && r >= 0 && r < n;
+
+            responses[i++] = prefixViews[r + 1] - prefixViews[l];
+        }
+
+        return responses;
+    }
+}
---
+++ Beyond cracking the coding interview/src/test/java/com/interviewprep/bctci/chapter_43_PrefixSum/Problem_43_1/ChannelViewsTest.java
@@ -0,0 +1,31 @@
+package com.interviewprep.bctci.chapter_43_PrefixSum.Problem_43_1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ChannelViewsTest {
+
+    @Test
+    public void channelViewsTest() {
+        ChannelViews channelViews = new ChannelViews();
+
+        int[] views = new int[]{5};
+        int[][] queries = new int[][]{
+                new int[]{0, 0}
+        };
+        int[] expectedResponses = new int[]{5};
+        Assertions.assertArrayEquals(expectedResponses, channelViews.getViews(views, queries));
+
+
+        views = new int[]{3, 5, 4, 8, 7, 2, 5, 3, 2, 3};
+        queries = new int[][]{
+                new int[]{0, 1},
+                new int[]{0, 5},
+                new int[]{5, 8},
+                new int[]{3, 3}
+        };
+        expectedResponses = new int[]{8, 29, 12, 8};
+        Assertions.assertArrayEquals(expectedResponses, channelViews.getViews(views, queries));
+
+    }
+}
